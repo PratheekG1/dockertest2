ba185bffff541ce87f2370999310766f1cc3d33f
Pratheekg1 <pratheekg99@gmail.com>
2022-03-09 10:25:42 +0530

    Addf

---
+++ dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:8
+EXPOSE 8080
+ADD target/My-Jenkin-Docker-app1.jar My-Jenkin-Docker-app1.jar
+ENTRYPOINT ["java","-jar","/My-Jenkin-Docker-app1.jar"]
---
+++ src/main/java/com/jd/MyJenkinDockerApp1Application.java
@@ -0,0 +1,13 @@
+package com.jd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJenkinDockerApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJenkinDockerApp1Application.class, args);
+	}
+
+}
